8a168648606a86a620d003c77755e9149d73e9d6
START9INE <claudchavez1118@gmail.com>
2022-10-19 21:59:40 -0400

    Refactored Code, Added a community solution for easy readability

--- Easy/romantoint.js
+++ Easy/romantoint.js
@@ -142,3 +142,32 @@ var romanToInt = function (s) {
 // is 1000 >= 100? (Yes)
 // so we add 1000 to num (num = 1994)
 
+
+
+
+
+// Below is a very readable and easy to understand code from LeetCode
+//Also using HashMap
+//
+//
+// var romanToInt = function(s) {
+    var map = {
+        'I' : 1,
+        'V' : 5,
+        'X' : 10,
+        'L' : 50,
+        'C' : 100,
+        'D' : 500,
+        'M' : 1000,
+    };
+    if(s.length === 1) return map[s[0]];
+    var res = 0;
+    for(var i = 0 ; i < s.length;i++){
+        
+        if(map[s[i]] < map[s[i+1]]){
+            res += (map[s[i+1]] - map[s[i]]);
+            i++;
+        }else res += map[s[i]]
+    }
+    return res;
+};
